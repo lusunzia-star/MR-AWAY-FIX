bcb8e59ed7d5c46119f83693239c278592e62875
lusunzia-star <lusunzia@gmail.com>
2026-09-08 18:38:04 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "mr-away-fix-v1";
+
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./supabaseClient.js",
+  "./manifest.json",
+  "./icon.svg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
+  );
+});
