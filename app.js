f2a5b72428c3c52aa8b1c02c246326d276b296a4
lusunzia-star <lusunzia@gmail.com>
2026-09-08 18:45:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4025,3 +4025,11 @@ async function initializeApp() {
 
 
 initializeApp();
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./service-worker.js")
+      .catch(error => {
+        console.error("Service Worker registration failed:", error);
+      });
+  });
+}
